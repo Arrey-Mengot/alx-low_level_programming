410eab5ad26d722f15c209059ba44a9e25e908c9
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-05 17:25:29 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - Entry Point
+  *Description: Prints some string
+  *Return 0 is success
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puszzle");
+	return (0);
+}
