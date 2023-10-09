6df5b7b2170dd16818dc467173719139e68f8d68
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-09 16:43:06 -0700

    remove white space for betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/** 
+/**
   *main - This is the entry point
-  *Description: Tells us weather a number is 
+  *Description: Tells us weather a number is
   *postive or negative
   *Return: sucess (0)
   *
@@ -11,7 +11,6 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
