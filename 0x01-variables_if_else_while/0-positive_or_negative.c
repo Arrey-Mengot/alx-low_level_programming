0941b0d5f59993cd65327182eb9c77fae6c32aa2
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-09 16:36:50 -0700

    fixed betty complains

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/** 
+  *main - This is the entry point
+  *Description: Tells us weather a number is 
+  *postive or negative
+  *Return: sucess (0)
+  *
+*/
 
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
@@ -14,11 +20,11 @@ int main(void)
 	printf("%d ", n);
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("is zero");
+		printf("is zero\n");
 	}
 	else
 	{
