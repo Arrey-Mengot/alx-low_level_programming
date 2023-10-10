9dcf22f9c75c027f07a88a8290547cd136a733d1
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-09 19:16:04 -0700

    task 9 update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,14 @@
 
 int main(void)
 {
-    int i, j;
+	int digit;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i; j < 10; j++)
-        {
-            putchar(i + '0');
-            putchar(',');
-            putchar(' ');
-            putchar(j + '0');
-        }
-    }
-    
-    return (0);
+	for (digit = '0'; digit < '10'; digit++)
+	{
+		putchar(digit);
+		putchar(',');
+		putchar(' ');
+	}
+
+	return (0);
 }
-
