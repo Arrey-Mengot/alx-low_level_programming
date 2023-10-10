f10830348592532e7beb7d96958f5fef1cdd4783
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-09 17:46:12 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: Prints the alphabet
+  *Return: Sucess (0)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' || c != q)
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
