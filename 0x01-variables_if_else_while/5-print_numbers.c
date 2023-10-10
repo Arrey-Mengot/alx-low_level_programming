c1125d7cd46d5f0c8bd779ba933181d7b1c7bd03
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-09 17:56:55 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: Prints numbers
+  *Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		printf("\d", n);
+	}
+
+	return (0);
+}
