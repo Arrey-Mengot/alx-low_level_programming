9307f715c2558aa0bd686136ce6575a402d9ea9d
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-10 16:02:33 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  *print_sign - function to proint sign
+  *@n: argument to function
+  *Description: Function that prints the sign of a number
+  *Return: 1 and + if positive, -1 and - if negative else 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
