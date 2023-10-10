0a058534953b8a76e707700d207b985fcbb72c6f
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-10 10:49:49 -0700

    task two done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *print_alphabet_x10 - uses _putchar to print alphabet
+  *Description: Function that prints alphabet in lwer case
+  *Return: Always 0 (sucess)
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
