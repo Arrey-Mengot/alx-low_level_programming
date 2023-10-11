4b4ec5de50b78af75c6a949c6330303bd74d0987
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-10 17:05:40 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *add- adds two ints
+  *@x: interger
+  *@y: interger
+  *Description: Function add two numbers
+  *Return: sum of two numbers
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
