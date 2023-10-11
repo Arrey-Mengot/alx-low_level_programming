dc6f3861c5cd7eae7715f438de29b1eabb4872bc
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-10 17:15:41 -0700

    Keep BETTY ALWAYS HAPPY

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,6 @@
 #include"main.h"
 #include <stdio.h>
+
 /**
  * print_to_98 - print n to 98 counts
  *           separated by comma, followed
@@ -13,6 +14,7 @@
 void print_to_98(int n)
 {
 	int x;
+
 	if (n > 98)
 		for (x = n; x > 98; x--)
 			printf("%d, ", x);
