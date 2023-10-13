d5711ad60dbb69a79ed65324b503e99edc943118
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-12 19:22:59 -0700

    task 4 fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,22 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: Arrey Mengot
+ */
+
 #include "main.h"
 
 /**
-  *print_most_numbers - prints numbers from 0-9
-  *Description: Prints numbers 0-9
-  *Return: 0 if sucess
-  */
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ */
 void print_most_numbers(void)
 {
 	int x;
 
 	for (x = 48; x <= 57; x++)
 	{
-		if (x != 50 || x != 52)
+		if (x != 50 && x != 52)
 			_putchar(x);
 	}
+
 	_putchar('\n');
 }
