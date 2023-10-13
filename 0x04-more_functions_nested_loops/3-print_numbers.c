5b4913afe5d5c16b2d3b60906a03d7643b3a7842
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-12 18:32:45 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "putchar.h"
+
+/**
+  *print_numbers - prints numbers from 0-9
+  *Description: Prints numbers 0-9
+  *Return: 0 if sucess
+  */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
