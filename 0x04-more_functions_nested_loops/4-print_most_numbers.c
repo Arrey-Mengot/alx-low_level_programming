bcefcdf8a3854f758086d37a8e67601e144cc1f5
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-12 19:11:49 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *print_most_numbers - prints numbers from 0-9
+  *Description: Prints numbers 0-9
+  *Return: 0 if sucess
+  */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (x != 50 || x != 52)
+			_putchar(x)
+	}
+	_putchar('\n');
+}
