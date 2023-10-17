f1d843dbc108b5ce4ab86f1ec91d3e28d8fa4f01
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-17 06:57:40 -0700

    removed spaces to keep betty happy

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
   *reset_to_98 - updates value of pointer to 98
-  *@*p: pointer to int
+  *@p: pointer to int
   *Description: takes pointer and updates value
   *Return: void
   */
- void reset_to_98(int *p)
+void reset_to_98(int *p)
 {
 	*p = 98;
 }
