60036de5d0956fc1d770511c23cde070cda414d7
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-17 09:25:28 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_rev - prints string in reverse
+  * @s: String to be printed
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int strlen = 0;
+	int c;
+
+	while (s[strlen] != '\0')
+		strlen++;
+	for (c = strlen; c >= 0; c--)
+		_putchar(s[c]);
+	_putchar('\n')
+}
