771540859fb22f18f208eb39584d33ac264c6bfc
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-25 15:23:05 -0700

    keep betty happy

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,9 +2,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion(char *s) - Func that prints a string
+ * _puts_recursion - Func that prints a string
  * @s: ptr
  * Return: void
+ * Description: prints string
  */
 
 void _puts_recursion(char *s)
@@ -12,7 +13,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 		putchar('\n');
