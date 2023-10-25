709d81f57130cc9ea26db9eeefcfffa63142109f
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-25 15:25:08 -0700

    keep betty happy

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,12 +4,13 @@
  * @s: String
  * Return: void
  */
+
 void _print_rev_recursion(char *s)
 {
-    if (*s)
-    {
-        _print_rev_recursion(s+1);
-        putchar(*s);
-    }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
 }
 
