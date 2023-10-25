5c1ff6576b1a28a53365761b67081ccd57f8212c
Arrey-mengot <mengotcollins@gmail.com>
2023-10-25 23:08:52 +0100

    renamed file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion(char *s) - Func that prints a string
+ * @s: ptr
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+    if (*s)
+    {
+        putchar(*s);
+        _puts_recursion(s+1);
+    }
+    else
+        putchar('\n');
+
+}
+
