43ba6f5b98851b90d35c8a5b79eeceb8db68eb3a
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-30 08:45:07 -0700

    fix task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
   * main - prints name of program
   * @argc: number of args(length of argv)
   * @argv: array of args
-  * Return: 1 for success
+  * Return: 0 for success
   */
-int main(int argc __attribute__((unused)), char *argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (1);
 }
