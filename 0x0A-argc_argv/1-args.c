156365bad38db8b4e5d2fc376a9832ee31868ac6
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-30 08:55:36 -0700

    task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prints the number of arguments
+  * @argc: number of args(length of argv)
+  * @argv: array of args
+  * Return: 0 for success
+  */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", args);
+	return (0);
+}
