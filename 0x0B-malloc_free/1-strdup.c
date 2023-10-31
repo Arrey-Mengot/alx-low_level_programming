9fd8080c1fbdb780c88b9cc09165678e837fab20
Arrey-Mengot <mengotcollins@gmail.com>
2023-10-31 15:55:15 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * _strdup - func that returns pointer to newly allocated
+  * space in memomory which contains a copy of the string 
+  * given as parameter
+  * @str: string to be passed as parameter
+  * Return: Null is size=0 or if it fails and
+  * 0 if it works
+  */
+
+char *_strdup(char *str)
+{
+	char *strout;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++);
+
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
+}
