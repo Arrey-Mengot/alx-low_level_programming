03d0e159508a747ad807be244b1ba87db74b4ccd
Arrey-Mengot <mengotcollins@gmail.com>
2023-11-09 07:22:06 -0800

    added va_end in task 1 and completed task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+  *print_strings - func that sums all argumens
+  *@n: number of arguments provided
+  *@...: variable number of arguments
+  *@separator: separates the numbers
+  *Return: nothing
+  */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i;
+	char *str;
+
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(arg, const char*);
+		if (separator == NULL)
+			printf("%s", str);
+		if (str == NULL)
+			printf("nil");
+		i == (n - 1) ? printf("%s", str) :  printf("%s%s", str, separator);
+	}
+
+	printf("\n");
+	va_end(arg);
+}
