b83b2dcc6c9c525f709fbaf07e2ddd94aab307f5
Arrey-Mengot <mengotcollins@gmail.com>
2023-11-09 06:05:36 -0800

    initial commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  *sum_them_all - func that sums all argumens
+  *@n: number of arguments provided
+  *@...: variable number of arguments
+  *Return: sum of arguments or 0
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	var_list arg;
+	int i, sum;
+
+	var_start(arg, n);
+	sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += var_arg(arg, int);
+
+	var_end(arg);
+
+	return (sum);
+}
