3a13a6a52c357d5dab398a644bde2828d40c7625
Arrey-Mengot <mengotcollins@gmail.com>
2023-11-09 06:09:46 -0800

    main.h added

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "main.h"
 
 /**
   *sum_them_all - func that sums all argumens
---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,6 @@
+#ifndef __HEADER__
+#define __HEADER__
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
