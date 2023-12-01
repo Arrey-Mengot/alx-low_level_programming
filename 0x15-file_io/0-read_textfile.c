92330c3710b4f903292d1d62a9735379773a513c
Arrey-Mengot <mengotcollins@gmail.com>
2023-12-01 12:46:04 -0800

    task 1 write to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+  * read_textfile - reads text file and prints to stdout
+  * filename: filename
+  * letters: Number of letters to be read
+  * Return: nothing
+  *
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nread, nwrite;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	nread = read(fd, buffer, letters);
+	if (nread == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	nwrite = write(STDOUT_FILENO, buffer, nread);
+	if (nwrite == -1 || nwrite != nread)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return nwrite;
+
+
+}
